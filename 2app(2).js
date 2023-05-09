a7e40eec2b0f1d3b2687b2744b99f4f77e0764ca
Samuel-Oluwole <132638577+Samuel-Oluwole@users.noreply.github.com>
2023-05-09 14:02:21 -0400

    app.js

--- 2app(2).js
+++ 2app(2).js
@@ -1,40 +1,10 @@
-// Import dependencies
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-// Create the Express app
-const app = express();
-
-// Set up the app middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Set up the app routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
 });
-
-// Set up the API routes
-app.use('/api/items', require('./routes/items'));
-
-// Connect to the database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/shoppinglist', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('Connected to the database');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error('Error connecting to the database: ' + err);
-});
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+server.listen(port,() => {
+console.log(`Server running at port `+port);
 });
